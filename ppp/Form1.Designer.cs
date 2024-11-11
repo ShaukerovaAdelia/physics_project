f644c997ebb5e39c367f3908872dc77c2f56d34f
SofiaChepurnykh <winxsofa@bk.ru>
2024-11-11 14:11:36 +0500

    ы

--- ppp/Form1.Designer.cs
+++ ppp/Form1.Designer.cs
@@ -47,18 +47,33 @@
             this.txtInitialTemperature = new System.Windows.Forms.TextBox();
             this.txtInitialVolume = new System.Windows.Forms.TextBox();
             this.txtFinalTemperature = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(81, 219);
+            this.btnCalculate.Location = new System.Drawing.Point(399, 19);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(147, 69);
+            this.btnCalculate.Size = new System.Drawing.Size(110, 48);
             this.btnCalculate.TabIndex = 0;
-            this.btnCalculate.Text = "button1";
+            this.btnCalculate.Text = "Построить график";
             this.btnCalculate.UseVisualStyleBackColor = true;
             this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click_1);
             // 
@@ -68,13 +83,16 @@
             this.chart2.ChartAreas.Add(chartArea4);
             legend4.Name = "Legend1";
             this.chart2.Legends.Add(legend4);
-            this.chart2.Location = new System.Drawing.Point(330, 28);
+            this.chart2.Location = new System.Drawing.Point(5, 18);
+            this.chart2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart2.Name = "chart2";
+            series4.BorderWidth = 5;
             series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series4.Legend = "Legend1";
-            series4.Name = "Series1";
+            series4.Name = "Изотермический";
             this.chart2.Series.Add(series4);
-            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.Size = new System.Drawing.Size(410, 307);
             this.chart2.TabIndex = 1;
             this.chart2.Text = "chart2";
             // 
@@ -84,13 +102,16 @@
             this.chart3.ChartAreas.Add(chartArea5);
             legend5.Name = "Legend1";
             this.chart3.Legends.Add(legend5);
-            this.chart3.Location = new System.Drawing.Point(707, 28);
+            this.chart3.Location = new System.Drawing.Point(5, 18);
+            this.chart3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart3.Name = "chart3";
+            series5.BorderWidth = 5;
             series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series5.Legend = "Legend1";
-            series5.Name = "Series1";
+            series5.Name = "Изохорный";
             this.chart3.Series.Add(series5);
-            this.chart3.Size = new System.Drawing.Size(300, 300);
+            this.chart3.Size = new System.Drawing.Size(410, 307);
             this.chart3.TabIndex = 2;
             this.chart3.Text = "chart3";
             // 
@@ -100,80 +121,206 @@
             this.chart4.ChartAreas.Add(chartArea6);
             legend6.Name = "Legend1";
             this.chart4.Legends.Add(legend6);
-            this.chart4.Location = new System.Drawing.Point(1098, 28);
+            this.chart4.Location = new System.Drawing.Point(5, 18);
+            this.chart4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart4.Name = "chart4";
+            series6.BorderWidth = 5;
             series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series6.Legend = "Legend1";
-            series6.Name = "Series1";
+            series6.Name = "Изобарный";
             this.chart4.Series.Add(series6);
-            this.chart4.Size = new System.Drawing.Size(300, 300);
+            this.chart4.Size = new System.Drawing.Size(410, 307);
             this.chart4.TabIndex = 3;
             // 
             // txtInitialPressure
             // 
-            this.txtInitialPressure.Location = new System.Drawing.Point(13, 28);
+            this.txtInitialPressure.Location = new System.Drawing.Point(28, 19);
+            this.txtInitialPressure.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtInitialPressure.Name = "txtInitialPressure";
-            this.txtInitialPressure.Size = new System.Drawing.Size(100, 22);
+            this.txtInitialPressure.Size = new System.Drawing.Size(76, 20);
             this.txtInitialPressure.TabIndex = 4;
             // 
             // txtFinalPressure
             // 
-            this.txtFinalPressure.Location = new System.Drawing.Point(158, 28);
+            this.txtFinalPressure.Location = new System.Drawing.Point(28, 47);
+            this.txtFinalPressure.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFinalPressure.Name = "txtFinalPressure";
-            this.txtFinalPressure.Size = new System.Drawing.Size(100, 22);
+            this.txtFinalPressure.Size = new System.Drawing.Size(76, 20);
             this.txtFinalPressure.TabIndex = 5;
             // 
             // txtFinalVolume
             // 
-            this.txtFinalVolume.Location = new System.Drawing.Point(158, 73);
+            this.txtFinalVolume.Location = new System.Drawing.Point(154, 47);
+            this.txtFinalVolume.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFinalVolume.Name = "txtFinalVolume";
-            this.txtFinalVolume.Size = new System.Drawing.Size(100, 22);
+            this.txtFinalVolume.Size = new System.Drawing.Size(76, 20);
             this.txtFinalVolume.TabIndex = 6;
             // 
             // txtInitialTemperature
             // 
-            this.txtInitialTemperature.Location = new System.Drawing.Point(13, 123);
+            this.txtInitialTemperature.Location = new System.Drawing.Point(280, 19);
+            this.txtInitialTemperature.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtInitialTemperature.Name = "txtInitialTemperature";
-            this.txtInitialTemperature.Size = new System.Drawing.Size(100, 22);
+            this.txtInitialTemperature.Size = new System.Drawing.Size(76, 20);
             this.txtInitialTemperature.TabIndex = 7;
             // 
             // txtInitialVolume
             // 
-            this.txtInitialVolume.Location = new System.Drawing.Point(13, 73);
+            this.txtInitialVolume.Location = new System.Drawing.Point(154, 19);
+            this.txtInitialVolume.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtInitialVolume.Name = "txtInitialVolume";
-            this.txtInitialVolume.Size = new System.Drawing.Size(100, 22);
+            this.txtInitialVolume.Size = new System.Drawing.Size(76, 20);
             this.txtInitialVolume.TabIndex = 8;
             // 
             // txtFinalTemperature
             // 
-            this.txtFinalTemperature.Location = new System.Drawing.Point(158, 123);
+            this.txtFinalTemperature.Location = new System.Drawing.Point(280, 47);
+            this.txtFinalTemperature.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFinalTemperature.Name = "txtFinalTemperature";
-            this.txtFinalTemperature.Size = new System.Drawing.Size(100, 22);
+            this.txtFinalTemperature.Size = new System.Drawing.Size(76, 20);
             this.txtFinalTemperature.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.txtFinalTemperature);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.txtInitialTemperature);
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.txtFinalVolume);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtInitialVolume);
+            this.groupBox1.Controls.Add(this.txtInitialPressure);
+            this.groupBox1.Controls.Add(this.txtFinalPressure);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(516, 77);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ввод данных:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(17, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "p1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(6, 50);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(17, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "p2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(132, 19);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(17, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "V1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(132, 50);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(17, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "V2";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(258, 22);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(17, 20);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "T1";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(258, 51);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(17, 20);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.Text = "T2";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chart2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(420, 330);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "График №1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chart3);
+            this.groupBox3.Location = new System.Drawing.Point(438, 95);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(420, 330);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "График №2";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.chart4);
+            this.groupBox4.Location = new System.Drawing.Point(864, 95);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(420, 330);
+            this.groupBox4.TabIndex = 13;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "График №3";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1481, 631);
-            this.Controls.Add(this.txtFinalTemperature);
-            this.Controls.Add(this.txtInitialVolume);
-            this.Controls.Add(this.txtInitialTemperature);
-            this.Controls.Add(this.txtFinalVolume);
-            this.Controls.Add(this.txtFinalPressure);
-            this.Controls.Add(this.txtInitialPressure);
-            this.Controls.Add(this.chart4);
-            this.Controls.Add(this.chart3);
-            this.Controls.Add(this.chart2);
-            this.Controls.Add(this.btnCalculate);
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Выполнили студентки группы ИСТНб-23-1 Чепурных Софья и Шаукерова Аделия";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -189,6 +336,16 @@
         private System.Windows.Forms.TextBox txtInitialTemperature;
         private System.Windows.Forms.TextBox txtInitialVolume;
         private System.Windows.Forms.TextBox txtFinalTemperature;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
     }
 }
 
